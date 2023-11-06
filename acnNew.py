9ab9d80b156bbda051aac0a2ef7cd6f4373c702d
Banabas Codes <112658393+BnbKe@users.noreply.github.com>
2023-11-06 07:22:36 -0500

    acnNew.py

--- acnNew.py
+++ acnNew.py
@@ -7,7 +7,7 @@ import toml
 
 
 # Set OpenAI API key using the SDK's dedicated method
-st.write("My OpenAI Key:", st.secrets["OPENAI_KEY"]["key"])
+OPENAI_API_KEY=st.secrets["openai"]["key"]
 #st.write(
    # os.environ["key"] == st.secrets["key"])
 
