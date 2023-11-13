1e4cfbc08cfc06b83b368695dbf1b6a761e1d8c6
Banabas Coding <112658393+BnbKe@users.noreply.github.com>
2023-11-12 19:31:51 -0500

    Update acnNew.py

--- acnNew.py
+++ acnNew.py
@@ -1,14 +1,33 @@
+import openai
+import datetime
 import os
 import streamlit as st
-import openai
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+import base64
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-import toml
 
 
 # Set OpenAI API key using the SDK's dedicated method
 api_key = st.secrets["OPENAI_API_KEY"]
 openai.api_key=api_key
+
+# Hide 'Made with Streamlit' footer
+st.markdown("""
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    </style>
+    """, unsafe_allow_html=True)
+
 
 # Set up the Streamlit app
 def main():
