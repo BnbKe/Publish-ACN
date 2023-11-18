8917a46669a7d8e70fa6b2e71499dac691f12c67
Banabas Coding <112658393+BnbKe@users.noreply.github.com>
2023-11-18 08:46:01 -0500

    Update acnNew.py
    Additional functionalities added!

--- acnNew.py
+++ acnNew.py
@@ -16,9 +16,14 @@ import requests
 from bs4 import BeautifulSoup
 
 
+# Set your OpenAI API key
 # Set OpenAI API key using the SDK's dedicated method
 api_key = st.secrets["OPENAI_API_KEY"]
 openai.api_key=api_key
+  # Replace with your OpenAI API key
+openai.api_key = api_key
+
+model = "gpt-4"
 
 # Hide 'Made with Streamlit' footer
 st.markdown("""
@@ -28,8 +33,100 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
+# Create directories for logs
+if not os.path.exists('Chat Logs'):
+    os.makedirs('Chat Logs')
+if not os.path.exists('Saved Chats'):
+    os.makedirs('Saved Chats')
 
-# Set up the Streamlit app
+# Generate log file path
+timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+log_file_path = os.path.join('Chat Logs', f'log_{timestamp}.txt')
+
+# Initialize typed query history in session state
+def scrape_google_scholar(query):
+    # Construct the URL for Google Scholar search
+    url = "https://scholar.google.com/scholar?q=" + "+".join(query.split())
+
+    headers = {'User-Agent': 'Mozilla/5.0'}  # Header to mimic a browser request
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Extract relevant data (titles, authors, etc.)
+        # This is an example and will need adjustment based on Google Scholar's HTML structure
+        titles = soup.find_all('h3', class_='gs_rt')
+        return [title.get_text() for title in titles]
+    else:
+        return "Failed to retrieve data"
+
+if 'typed_query_history' not in st.session_state:
+    st.session_state.typed_query_history = []
+
+def generate_bar_chart(df, column):
+    fig = px.bar(df, x=column, title=f'Bar Chart of {column}')
+    st.plotly_chart(fig)
+
+def generate_histogram(df, column):
+    fig = px.histogram(df, x=column, title=f'Histogram of {column}')
+    st.plotly_chart(fig)
+
+def show_advanced_stats(df):
+    st.write("Skewness of each column:")
+    st.write(df.skew())
+    st.write("Kurtosis of each column:")
+    st.write(df.kurtosis())
+
+def build_linear_regression_model(df):
+    st.sidebar.subheader("Linear Regression Model")
+
+    # Filter DataFrame to numeric columns only
+    numeric_columns = df.select_dtypes(include=[np.number]).columns
+
+    if len(numeric_columns) > 1:
+        x_column = st.sidebar.selectbox("Select Feature Column", numeric_columns)
+        y_column = st.sidebar.selectbox("Select Target Column", numeric_columns)
+        X = df[[x_column]]
+        y = df[y_column]
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        model = LinearRegression()
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+
+        mse = mean_squared_error(y_test, y_pred)
+        r2 = r2_score(y_test, y_pred)
+
+        st.write("Mean Squared Error:", mse)
+        st.write("R^2 Score:", r2)
+    else:
+        st.sidebar.write("Not enough numeric columns for regression analysis.")
+
+def generate_seaborn_plot(df):
+    st.sidebar.subheader("Seaborn Plot Options")
+    plot_type = st.sidebar.selectbox("Select Plot Type", ["Pairplot", "Heatmap", "Boxplot"])
+    
+    if plot_type == "Pairplot":
+        pairplot = sns.pairplot(df)
+        fig = pairplot.fig
+    elif plot_type == "Heatmap":
+        fig, ax = plt.subplots()
+        sns.heatmap(df.corr(), ax=ax)
+    elif plot_type == "Boxplot":
+        selected_column = st.sidebar.selectbox("Choose Column for Boxplot", df.columns)
+        fig, ax = plt.subplots()
+        sns.boxplot(x=df[selected_column], ax=ax)
+
+    st.pyplot(fig)
+
+def download_csv(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="data.csv">Download CSV File</a>'
+    st.markdown(href, unsafe_allow_html=True)
+
+
 def main():
     st.title('ACN GPT Analyst')
     st.sidebar.image("ACN_LOGO.webp", caption='ACN', use_column_width=True)
