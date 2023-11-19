a100220e45491f5c4cd5f4314ff7b7339cb58c1d
Banabas Coding <112658393+BnbKe@users.noreply.github.com>
2023-11-18 21:34:50 -0500

    Update acnNew.py

--- acnNew.py
+++ acnNew.py
@@ -38,12 +38,11 @@ log_file_path = os.path.join('Chat Logs', f'log_{timestamp}.txt')
 if 'typed_query_history' not in st.session_state:
     st.session_state.typed_query_history = []
 
-# Google Scholar scraping function
+# Google Scholar scraping function for peer-reviewed research
 def scrape_google_scholar(query):
     url = "https://scholar.google.com/scholar?q=" + "+".join(query.split())
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers)
-
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
         titles = soup.find_all('h3', class_='gs_rt')
@@ -69,21 +68,17 @@ def show_advanced_stats(df):
 def build_linear_regression_model(df):
     st.sidebar.subheader("Linear Regression Model")
     numeric_columns = df.select_dtypes(include=[np.number]).columns
-
     if len(numeric_columns) > 1:
         x_column = st.sidebar.selectbox("Select Feature Column", numeric_columns)
         y_column = st.sidebar.selectbox("Select Target Column", numeric_columns)
         X = df[[x_column]]
         y = df[y_column]
-
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
         model = LinearRegression()
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
-
         mse = mean_squared_error(y_test, y_pred)
         r2 = r2_score(y_test, y_pred)
-
         st.write("Mean Squared Error:", mse)
         st.write("R^2 Score:", r2)
     else:
@@ -92,7 +87,6 @@ def build_linear_regression_model(df):
 def generate_seaborn_plot(df):
     st.sidebar.subheader("Seaborn Plot Options")
     plot_type = st.sidebar.selectbox("Select Plot Type", ["Pairplot", "Heatmap", "Boxplot"])
-
     if plot_type == "Pairplot":
         pairplot = sns.pairplot(df)
         fig = pairplot.fig
@@ -103,7 +97,6 @@ def generate_seaborn_plot(df):
         selected_column = st.sidebar.selectbox("Choose Column for Boxplot", df.columns)
         fig, ax = plt.subplots()
         sns.boxplot(x=df[selected_column], ax=ax)
-
     st.pyplot(fig)
 
 def download_csv(df):
@@ -115,29 +108,50 @@ def download_csv(df):
 # Main function with multi-page setup
 def main():
     st.sidebar.image("ACN_LOGO.webp", caption='ACN', use_column_width=True)
-    page = st.sidebar.radio('Choose a section', ['Home', 'Data Analysis', 'Research', 'Chatbot', 'Query History'])
+    page = st.sidebar.radio('Choose a section', ['Home with Chatbot', 'Data Analysis', 'Peer-reviewed Research', 'Query History'])
 
-    if page == 'Home':
-        display_home()
+    if page == 'Home with Chatbot':
+        display_home_with_chatbot()
     elif page == 'Data Analysis':
         display_data_analysis()
-    elif page == 'Research':
-        display_research()
-    elif page == 'Chatbot':
-        display_chatbot()
+    elif page == 'Peer-reviewed Research':
+        display_peer_reviewed_research()
     elif page == 'Query History':
         display_query_history()
 
-def display_home():
-    st.title('Welcome to ACN GPT Analyst')
+def display_home_with_chatbot():
+    st.title('Welcome to ACN GPT Analyst - Chatbot')
+    handle_chatbot_queries()
 
 def display_data_analysis():
     st.title('Data Analysis Tools')
-    # Implement data analysis tools here
+    data_file = st.file_uploader("Upload a Dataset", type=['csv', 'xlsx'])
+    if data_file is not None:
+        if data_file.type == "text/csv":
+            df = pd.read_csv(data_file)
+        elif data_file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+            df = pd.read_excel(data_file)
+        if st.checkbox('Show Dataset'):
+            st.write(df)
+        st.subheader("Data Visualization Tools")
+        try:
+            column = st.selectbox("Select Column for Analysis", df.columns)
+            if st.button('Generate Bar Chart'):
+                generate_bar_chart(df, column)
+            if st.button('Generate Histogram'):
+                generate_histogram(df, column)
+            if st.button('Show Advanced Statistics'):
+                show_advanced_stats(df)
+            if st.button('Build Linear Regression Model'):
+                build_linear_regression_model(df)
+            if st.button('Generate Seaborn Plot'):
+                generate_seaborn_plot(df)
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
 
-def display_research():
-    st.title('Research')
-    query = st.text_input('Enter your research query:')
+def display_peer_reviewed_research():
+    st.title('Peer-reviewed Research')
+    query = st.text_input('Enter your research query for peer-reviewed articles:')
     if query:
         results = scrape_google_scholar(query)
         if results != "Failed to retrieve data":
@@ -145,14 +159,6 @@ def display_research():
                 st.write(title)
         else:
             st.error("Failed to retrieve data from Google Scholar")
-
-def display_chatbot():
-    st.title('Chatbot')
-    handle_chatbot_queries()
-
-def display_query_history():
-    st.title('Query History')
-    display_typed_query_history()
 
 def handle_chatbot_queries():
     user_query = st.text_input('Ask anything about Africans in the US:', '')
@@ -164,6 +170,10 @@ def handle_chatbot_queries():
         for source_data in response_data["responses"]:
             st.write(source_data['response'])
         st.session_state.typed_query_history.append(response_data)
+
+def display_query_history():
+    st.title('Query History')
+    display_typed_query_history()
 
 def display_typed_query_history():
     clear_typed_query_history = st.sidebar.button("Clear Typed Query History")
